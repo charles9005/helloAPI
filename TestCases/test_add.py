af35032fba5446163b62c6383027ff297a5bf8cd
Charles.Lin <626853099@qq.com>
2022-06-19 16:32:48 +0800

    新增hello模块测试

--- TestCases/test_add.py
+++ TestCases/test_add.py
@@ -1,11 +1,3 @@
-"""
-======================
-Author: 柠檬班-小简
-Time: 2020/7/20 20:20
-Project: day10
-Company: 湖南零檬信息技术有限公司
-======================
-"""
 import unittest
 
 import allure
---
+++ TestCases/test_hello.py
@@ -0,0 +1,9 @@
+import pytest
+import allure
+
+@allure.story("测试hello模块")
+class TestHello:
+    def test_h1(self):
+        pass
+    def test_h2(self):
+        pass
